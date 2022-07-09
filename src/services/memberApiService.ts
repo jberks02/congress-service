dfa66eacd64aade67c8798c87df1d243e5afdf3f
Justin Berkshire <justinberkshire@Justins-MacBook-Pro.local>
2022-07-08 22:35:16 -0500

    api Services complete

--- src/services/memberApiService.ts
+++ src/services/memberApiService.ts
@@ -1,4 +1,5 @@
 import fetch, { RequestInit, Response } from 'node-fetch';
+import { reducedChamber, stateAbbreviation } from '../types/request';
 const { propublica_key } = process.env;
 
 export default class MemberApi {
@@ -10,5 +11,30 @@ export default class MemberApi {
         this.urlBase = 'https://api.propublica.org';
         this.header ={ method: 'GET', 'headers': { 'X-API-Key': this.pass } }
     }
-    
+    async listOfMembers(congress: number, chamber: reducedChamber): Promise<members.request['results']> {
+
+        const url = new URL(`/congress/v1/${congress}/${chamber}/members.json`, this.urlBase);
+
+        const req: Response = await fetch(url.href, this.header);
+
+        if(!req.ok) throw Error(await req.text());
+
+        const res = await req.json() as members.request;
+
+        return res.results;
+
+    }
+    async membersByStateId(chamber: reducedChamber, state: stateAbbreviation): Promise<currentMembersByStateOrDistrict.result['results']> {
+
+        const url = new URL(`/congress/v1/members/${chamber}/${state}/current.json`, this.urlBase);
+
+        const req: Response = await fetch(url.href, this.header);
+
+        if(!req.ok) throw Error(await req.text());
+
+        const res = await req.json() as currentMembersByStateOrDistrict.result;
+
+        return res.results;
+
+    }
 }
--- src/types/request.d.ts
+++ src/types/request.d.ts
@@ -3,3 +3,60 @@ export type chamber = 'senate' | 'house' | 'both';
 export type reducedChamber = 'senate' | 'house';
 
 export type updateType = 'introduced' | 'updated' | 'active' | 'passed' | 'enacted' | 'vetoed'
+
+export type stateAbbreviation = 'AL' | 'AK' | 'AZ' | 'AR' | 'AS' | 'CA' | 'CO' | 'CT' | 'DE' | 'DC' | 'FL' | 'VT' | 'VA' | 'WA' | 'WV' | 'WI' | 'GA' | 'GU' | 'HI' | 'ID' | 'IL' | 'IN' | 'IA' | 'KS' | 'NV' | 'NH' | 'NJ' | 'NM' | 'NY' | 'NC' | 'ND' | 'WY' | 'KY' | 'LA' | 'ME' | 'MD' | 'MA' | 'MI' | 'MN' | 'MS' | 'MO' | 'MT' | 'NE' | 'OH' | 'OK' | 'OR' | 'PA' | 'PR' | 'RI' | 'SC' | 'SD' | 'TN' | 'TX' | 'UT'
+
+// Alabama	        AL	
+// Alaska	        AK	
+// Arizona	        AZ	
+// Arkansas	        AR	
+// American Sa      AS	
+// California	    CA	
+// Colorado 	    CO	
+// Connecticut	    CT	
+// Delaware	        DE	
+// Capital  	    DC	
+// Florida	        FL	
+// Vermont	        VT
+// Virginia	        VA
+// Washington	    WA
+// West Virginia	WV
+// Wisconsin	    WI
+// Georgia	        GA	
+// Guam	            GU	
+// Hawaii	        HI	
+// Idaho	        ID	
+// Illinois	        IL	
+// Indiana	        IN	
+// Iowa	            IA	
+// Kansas	        KS  
+// Nevada	        NV	
+// New Hampshire	NH	
+// New Jersey	    NJ	
+// New Mexico	    NM	
+// New York	        NY	
+// North Carolina	NC	
+// North Dakota	    ND	
+// Wyoming	        WY
+// Kentucky	        KY	
+// Louisiana	    LA	
+// Maine	        ME	
+// Maryland	        MD	
+// Massachusetts	MA	
+// Michigan	        MI	
+// Minnesota	    MN	
+// Mississippi	    MS	
+// Missouri	        MO	
+// Montana	        MT	
+// Nebraska	        NE	
+// Ohio	            OH
+// Oklahoma	        OK
+// Oregon	        OR
+// Pennsylvania	    PA
+// Puerto Rico	    PR
+// Rhode Island	    RI
+// South Carolina	SC
+// South Dakota	    SD
+// Tennessee	    TN
+// Texas	        TX
+// Utah	            UT
